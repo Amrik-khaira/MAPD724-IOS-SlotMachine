b66ca105611242021ff646727ac8f84736d67ce5
Amrik Singh <113277554+Amrik-khaira@users.noreply.github.com>
2023-01-21 06:25:39 -0500

    Add files via upload

--- Slot Machine/ViewController.swift
+++ Slot Machine/ViewController.swift
@@ -11,33 +11,6 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        guard let confettiImageView = UIImageView.fromGif(frame: view.frame, resourceName: "confetti") else { return }
-        view.addSubview(confettiImageView)
-        confettiImageView.startAnimating()
-       
-    }
-
-
-}
-
-extension UIImageView {
-    static func fromGif(frame: CGRect, resourceName: String) -> UIImageView? {
-        guard let path = Bundle.main.path(forResource: resourceName, ofType: "gif") else {
-            print("Gif does not exist at that path")
-            return nil
-        }
-        let url = URL(fileURLWithPath: path)
-        guard let gifData = try? Data(contentsOf: url),
-            let source =  CGImageSourceCreateWithData(gifData as CFData, nil) else { return nil }
-        var images = [UIImage]()
-        let imageCount = CGImageSourceGetCount(source)
-        for i in 0 ..< imageCount {
-            if let image = CGImageSourceCreateImageAtIndex(source, i, nil) {
-                images.append(UIImage(cgImage: image))
-            }
-        }
-        let gifImageView = UIImageView(frame: frame)
-        gifImageView.animationImages = images
-        return gifImageView
+      
     }
 }
